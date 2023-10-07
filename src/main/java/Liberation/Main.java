7a9dd1f3c751d5dfcddb89a54df76e7eaf5ba36c
Lioninn <ryandinaror@gmail.com>
2023-10-07 17:00:05 -0600

    Make into gradle project

--- src/main/java/Liberation/Main.java
+++ src/main/java/Liberation/Main.java
@@ -6,7 +6,7 @@ import Liberation.SecureInformation.UserManagement;
 public class Main {
     public static void main(String[] args) {
         UserManagement management = new UserManagement(null);
-        StaffLoginSystem loginSystem = new StaffLoginSystem(null);
+        //StaffLoginSystem loginSystem = new StaffLoginSystem(null);
         for(int x = 0; x < 100;x++) {
             //System.out.println(management.generateTempPassword());
         }
